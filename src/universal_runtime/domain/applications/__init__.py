d5c031e926034d4156536e569c11e300d22a0bf5
Furkan Zeki ÖZYURT <fzozyurt@outlook.com>
2026-07-20 03:07:23 +0300

    feat: export application control-plane contracts

---
+++ src/universal_runtime/domain/applications/__init__.py
@@ -0,0 +1,11 @@
+from universal_runtime.domain.applications.entities import (
+    ApplicationDeploymentRegistration,
+    GraphRegistration,
+    ResolvedExecutionPlan,
+)
+
+__all__ = [
+    "ApplicationDeploymentRegistration",
+    "GraphRegistration",
+    "ResolvedExecutionPlan",
+]
